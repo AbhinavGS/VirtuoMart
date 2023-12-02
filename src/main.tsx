b1fcfd6c2c82b970a6a5e9e78a15e93b07c7c67a
Abhinav Sorate <abhinav.sorate@gida.io>
2023-12-02 21:17:30 +0530

    setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store.ts";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
